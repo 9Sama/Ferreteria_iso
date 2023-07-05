1653cdc7be7536f14619c589a61e37be75faccf8
Sama <ssamana@unitru.edu.pe>
2023-07-04 23:06:07 -0500

    probando commit

--- database/base.sql
+++ database/base.sql
@@ -1,7 +1,13 @@
-DROP DATABASE IF EXISTS ferreteria;
-create database ferreteria;
-use ferreteria;
+DROP DATABASE IF EXISTS ferreteriap;
+create database ferreteriap;
+use ferreteriap;
+
+-- Sub_Personal
 create table area(idarea int primary key AUTO_INCREMENT,nombre varchar(30),descripcion varchar(40),estado tinyint);
 create table puesto(idpuesto int primary key AUTO_INCREMENT,idarea int, nombre varchar(30),descripcion varchar(40),estado tinyint, foreign key(idarea)references area(idarea));
 create table plaza(idplaza int primary key AUTO_INCREMENT, idpuesto int, cantidad int,foreign key(idpuesto)references puesto(idpuesto));
 create table postulante(idpostulante int primary key AUTO_INCREMENT, dni char(9), apellidos varchar(80), nombres varchar(80),gradoEstudios varchar(80),centroEstudios varchar(100),estado tinyint);
+
+
+-- Sub_Ventas
+create table cliente(idcliente int primary key AUTO_INCREMENT, dni int(8), apellidos varchar(40), nombres varchar(40),direccion varchar(60),telefono int(08),celular int(09))
--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-    <title>Gentelella Alela!</title>
+    <title>Ferretería UNT</title>
 
     <!-- Bootstrap -->
     <link href="/adminlte/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">

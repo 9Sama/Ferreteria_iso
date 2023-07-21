544cb83ac45f4cc6c1f65d73076bc17b6d186b89
Jhon Livias <jhonlivias3@gmail.com>
2023-07-21 12:49:11 -0500

    modulo sub_personal

--- database/base.sql
+++ database/base.sql
@@ -2,12 +2,17 @@ DROP DATABASE IF EXISTS ferreteriap;
 create database ferreteriap;
 use ferreteriap;
 
-create table area(idarea int primary key AUTO_INCREMENT,nombre varchar(30),descripcion varchar(40),estado tinyint);
-create table puesto(idpuesto int primary key AUTO_INCREMENT,idarea int, nombre varchar(30),descripcion varchar(40),estado tinyint, foreign key(idarea)references area(idarea));
-create table plaza(idplaza int primary key AUTO_INCREMENT, idpuesto int, cantidad int,foreign key(idpuesto)references puesto(idpuesto));
-create table postulante(idpostulante int primary key AUTO_INCREMENT, dni char(9), apellidos varchar(80), nombres varchar(80),gradoEstudios varchar(80),centroEstudios varchar(100),estado tinyint);
+drop table if exists plaza;
+drop table if exists puesto;
+drop table if exists area;
+drop table if exists postulante;
+drop table if exists personal;
 
-create table cliente(idcliente int primary key AUTO_INCREMENT,dni char(8),apellidos varchar(40),nombres varchar(40),direccion varchar(60),telefono char(08),celular char(09));
+create table area(idarea int primary key AUTO_INCREMENT,nombre varchar(30),descripcion varchar(50),estado tinyint);
+create table puesto(idpuesto int primary key AUTO_INCREMENT,idarea int, nombre varchar(30),descripcion varchar(40),estado tinyint, foreign key(idarea)references area(idarea));
+create table plaza(idplaza int primary key AUTO_INCREMENT, idpuesto int, cantidad int, convocatoria text,foreign key(idpuesto)references puesto(idpuesto));
+create table postulante(idpostulante int primary key AUTO_INCREMENT, dni char(8), apellidos varchar(35), nombres varchar(40),direccion varchar(30),fechanac date,celular char(9), gradoEstudios varchar(40),centroEstudios varchar(45));
+create table personal(idpersonal int primary key AUTO_INCREMENT, dni char(8), apellidos varchar(35), nombres varchar(40),direccion varchar(30),fechanac date,celular char(9),gradoEstudios varchar(40),centroEstudios varchar(45),sueldo float,fechaIng date, idpuesto int, foreign key(idpuesto)references puesto(idpuesto));
 
 create table categoria(
     idcategoria int not null AUTO_INCREMENT,
@@ -94,4 +99,3 @@ create table detalle_venta(
     FOREIGN KEY (idventa) REFERENCES venta (idventa) ON DELETE CASCADE,
     FOREIGN KEY (idarticulo) REFERENCES articulo (idarticulo)
 );
-

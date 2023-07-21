34b24791c7e797ef0828cc4b45313855dbee9e63
Jhon Livias <jhonlivias3@gmail.com>
2023-07-21 02:11:50 -0500

    Modulo de ventas

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\CompraController;
 use App\Http\Controllers\ProveedorController;
 use App\Http\Controllers\PostulanteController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\VentaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,6 +42,12 @@ Route::post('personas/proveedor', [ProveedorController::class, 'store'])->name('
 Route::put('personas/proveedor/{id}', [ProveedorController::class, 'update'])->name('proveedores.update');
 Route::delete('personas/proveedor/{id}', [ProveedorController::class, 'destroy'])->name('proveedores.delete');
 
+// Cliente
+Route::get('personas/cliente', [CustomerController::class, 'index'])->name('clientes.index');
+Route::post('personas/cliente', [CustomerController::class, 'store'])->name('clientes.store');
+Route::put('personas/cliente/{id}', [CustomerController::class, 'update'])->name('clientes.update');
+Route::delete('personas/cliente/{id}', [CustomerController::class, 'destroy'])->name('clientes.delete');
+
 // Compras
 Route::get('compras', [CompraController::class, 'index'])->name('compras.index');
 Route::get('compras/registrar', [CompraController::class, 'create'])->name('compras.create');
@@ -47,3 +55,11 @@ Route::post('compras/guardar', [CompraController::class, 'store'])->name('compra
 Route::get('compras/mostrar/{id}', [CompraController::class, 'show'])->name('compras.show');
 Route::put('compras/aprobar/{id}', [CompraController::class, 'approve'])->name('compras.approve');
 Route::put('compras/rechazar/{id}', [CompraController::class, 'reject'])->name('compras.reject');
+
+// Ventas
+Route::get('ventas', [VentaController::class, 'index'])->name('ventas.index');
+Route::get('ventas/registrar', [VentaController::class, 'create'])->name('ventas.create');
+Route::post('ventas/guardar', [VentaController::class, 'store'])->name('ventas.store');
+Route::get('ventas/mostrar/{id}', [VentaController::class, 'show'])->name('ventas.show');
+Route::put('ventas/aprobar/{id}', [VentaController::class, 'approve'])->name('ventas.approve');
+Route::put('ventas/rechazar/{id}', [VentaController::class, 'reject'])->name('ventas.reject');
